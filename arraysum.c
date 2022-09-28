ac33556615ebb99d44169b89259700c94f0a57c2
NamanZadoo <114582558+NamanZadoo@users.noreply.github.com>
2022-09-28 17:20:15 +0530

    Add files via upload

---
+++ arraysum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a[5],i,sum=0;
+    for(i=0;i<=4;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=4;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
