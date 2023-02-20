2e36303f94bc89aaec402b5f261861bf99832433
Naman Zadoo <114582558+NamanZadoo@users.noreply.github.com>
2023-02-20 11:34:09 +0530

    Create median_quick_sort.c

---
+++ sort/median_quick_sort.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+/****************/
+int partition(int a[],int low, int high)
+{
+    int med=(low+high)/2;
+    int t;
+    t=a[low];
+    a[low]=a[med];
+    a[med]=t;
+    int i=low,j=high;
+    int pivot=a[low];
+    int c;
+    do
+    {
+        do
+        {
+            i++;
+        } while (a[i]<pivot);
+        do
+        {
+            j--;
+        } while (a[j]>pivot);
+        if(i<j)
+        {
+            c=a[i];
+            a[i]=a[j];
+            a[j]=c;
+        }
+        
+    }while(i<j);
+
+    c=a[j];
+    a[j]=a[low];
+    a[low]=c;
+    return j;
+}
+/****************/
+void quicksort(int a[],int low,int high)
+{
+    int j;
+    if(low<high)
+    {
+        j=partition(a,low,high);
+        quicksort(a,low,j-1);
+        quicksort(a,j+1,high);
+    }
+}
+/****************/
+int main()
+{
+    int a[100];
+    int i,n;
+    printf("enter size of array:\t ");
+    scanf("%d",&n);
+    a[n]=__INT_MAX__;
+    printf("enter the elemtnts:\t");
+    for(i=0;i<=n-1;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    quicksort(a,0,n);
+    for(i=0;i<=n-1;i++)
+    {
+        printf(" %d ",a[i]);
+    }
+    return 0;
+}
